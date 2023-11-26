5c61c9cc582b5814bb6319035bf21c472ac59541
gabrielmmart <gabriel.medeiros@ges.inatel.br>
2023-11-26 18:09:23 -0300

    tratamento KPI frontend

--- frontend/src/components/MainPage/Main.jsx
+++ frontend/src/components/MainPage/Main.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './Main.css';
 import Profile from '../Perfil/PerfilComponent' 
 import axios from 'axios';
-
+import { useUser } from '../UserContextF/UserContext';
 
 const Main = () => {
-
+    const { user } = useUser();
     const [atalhos, setAtalhos] = useState([]);
 
     useEffect(() => {
@@ -20,27 +20,29 @@ const Main = () => {
     }, []);
 
     return (
-        <section className="main container section">
-            
-            <Profile/>
-
-            <div className="secContent">
-            {atalhos &&
+      <section className="main container section">
+        <Profile />
+        <div className="secContent">
+          {atalhos &&
             atalhos.map(({ _id, imgSrc, nomeApp, link }) => (
-              <div key={_id} className="singleDestination">
-                <div className="imageDiv">
-                  <a href={link} target="_blank" rel="noopener noreferrer">
-                    <img src={imgSrc} alt={nomeApp} />
-                  </a>
+              // Check if the shortcut is for KPI and the user has acessoKPI
+              (nomeApp === 'KPI' && user.user.acessoKPI) || nomeApp !== 'KPI' ? (
+                <div key={_id} className="singleDestination">
+                  <div className="imageDiv">
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      <img src={imgSrc} alt={nomeApp} />
+                    </a>
+                  </div>
+                  <div className="nomeDiv" style={{ color: "white" }}>
+                    <h4>{nomeApp}</h4>
+                  </div>
                 </div>
-                <div className="nomeDiv" style={{ color: "white" }}>
-                  <h4>{nomeApp}</h4>
-                </div>
-              </div>
+              ) : null
             ))}
         </div>
-        </section>
+      </section>
     );
+    
 }
 
 export default Main;
